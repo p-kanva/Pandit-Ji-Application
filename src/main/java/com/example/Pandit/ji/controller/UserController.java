51d745ed217b9aa39d35ca6ec1768df7e56a5fa6
Prathamesh <p.kanva@gmail.com>
2025-09-20 15:58:47 +0530

    JWT changes

--- src/main/java/com/example/Pandit/ji/controller/UserController.java
+++ src/main/java/com/example/Pandit/ji/controller/UserController.java
@@ -2,10 +2,15 @@ package com.example.Pandit.ji.controller;
 
 
 import com.example.Pandit.ji.Model.User;
+import com.example.Pandit.ji.Util.JwtUtil;
+import com.example.Pandit.ji.repo.UserRepository;
 import com.example.Pandit.ji.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,15 +19,21 @@ import java.util.List;
 @RequestMapping("api/users")
 public class UserController {
 
-
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
-    public UserController(UserService userService){
+
+    public UserController(UserService userService, AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserRepository userRepository) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@Valid @RequestBody User user){
+    public ResponseEntity<User> registerUser(@Valid @RequestBody User user) {
 
         User savedUser = userService.registerUser(user);
         return ResponseEntity.ok(savedUser);
@@ -30,13 +41,27 @@ public class UserController {
 
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getName(), user.getPassword()));
+            User u = userRepository.findByUser(user.getName()).orElseThrow();
+            String token=jwtUtil.generateToken(u.getName(),u.getUserType().name());
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     @GetMapping
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id){
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return userService.getUserById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
